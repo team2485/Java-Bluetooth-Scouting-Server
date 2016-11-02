57d74aabc6d178f0bef2b0a10298d04b37dd6d5b
Sahana-Kumar <skumar2019@francisparker.org>
2016-11-01 21:42:13 -0700

    Get Method ... most likely does not work

--- src/org/first/team2485/scoutingserver/Server.java
+++ src/org/first/team2485/scoutingserver/Server.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Server {
 	
@@ -18,7 +21,32 @@ public class Server {
 							FileReader fileReader = new FileReader(curFile);
 							BufferedReader bufferedReader = new BufferedReader(fileReader);
 							String scoutingData = bufferedReader.readLine();
-						} catch (IOException e) {
+							
+							String url = ""; // set this at some point
+							URL obj = new URL(url);
+							HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+							con.setRequestMethod("GET");
+							con.setRequestProperty("Send Scouting Data", scoutingData);
+							
+							int responseCode = con.getResponseCode();
+							System.out.println("\nSending 'GET' request to URL : " + url);
+							System.out.println("Response Code : " + responseCode);
+
+							BufferedReader in = new BufferedReader(
+							        new InputStreamReader(con.getInputStream()));
+							String inputLine;
+							StringBuffer response = new StringBuffer();
+
+							while ((inputLine = in.readLine()) != null) {
+								response.append(inputLine);
+							}
+							in.close();
+
+							//print result
+							System.out.println(response.toString());
+
+						}
+						catch (IOException e) {
 							e.printStackTrace();
 						}
 					}
